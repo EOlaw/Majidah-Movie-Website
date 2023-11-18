e8c4dd0fcbe0a912d4cc003370d5c20ff212c20e
EOlaw <Emmanuel.ao@outlook.com>
2023-11-17 23:42:41 -0600

    install mongoose on dependency

--- server.js
+++ server.js
@@ -79,5 +79,5 @@ app.use('/', userRoutes)
 const port = process.env.PORT || 3000;
 // Start the server
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+  console.log(`Server started on ${port}`);
 });
