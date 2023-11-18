62820404f27391e2d41fcedf78b040b5f4b1acde
EOlaw <Emmanuel.ao@outlook.com>
2023-11-17 23:04:42 -0600

    ready to try deploying

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 // Import required modules
 const express = require('express');
 const app = express();  // Create an instance of the Express app
@@ -6,12 +10,11 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('passport-local');
-const session = require('express-session');
-
 const User = require('./models/userModels')
-
+const dbUrl = process.env.DB_URL
 
 const homeRoutes = require('./routes/homeRoutes')
 const movieRoutes = require('./routes/movieRoutes')
@@ -19,8 +22,9 @@ const tvShowRoutes = require('./routes/tvshowRoutes')
 const userRoutes = require('./routes/userRoutes')
 
 
+
 // Set up the database connection
-mongoose.connect('mongodb+srv://EOlaw146:Olawalee_.146@cluster0.4wv68hn.mongodb.net/movias?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 // Check for database connection errors
 db.on("error", console.error.bind(console, "connection error:"));
@@ -74,6 +78,6 @@ app.use('/', userRoutes)
 
 
 // Start the server
-app.listen(3000, () => {
+app.listen(4500, () => {
   console.log('Server started on port 3000');
 });
